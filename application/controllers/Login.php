5302d23fdd074913ec9473d8362295e2ccd041bc
Tim Joosten <Tjoosten3@gmail.com>
2016-12-15 09:58:45 +0100

    St§art adding authencation

---
+++ application/controllers/Login.php
@@ -0,0 +1,154 @@
+<?php defined('BASEPATH') or exit ('No direct script access allowed');
+
+/**
+ * Login controller.
+ *
+ * @author      Tim Joosten
+ * @license     MIT License
+ * @since       2016
+ * @package     Activisme-BE Index Site
+ */
+class Login extends MY_Controller
+{
+    // FIXME: Add translation files. TO get the flash session work.
+    // FIXME: Add login view to the system.
+    // FIXME: Add authencation create view.
+    // FIXME: Add the logged in middleware. 
+
+    /**
+     * Authencated user session.
+     *
+     * @var array
+     */
+    public $User = [];
+
+    /**
+     * Login constructor.
+     *
+     * @return void.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library(['session', 'blade', 'form_validation']);
+        $this->load->helper(['string', 'url', 'language']);
+
+        $this->User = $this->session->userdata('logged_in');
+    }
+
+    /**
+     * only create if you want to use, not compulsory.
+     * or return parent::middleware(); if you want to keep.
+     * or return empty array() and no middleware will run.
+     */
+    protected function middleware()
+    {
+        /**
+         * Return the list of middlewares you want to be applied,
+         * Here is list of some valid options
+         *
+         * admin_auth                    // As used below, simplest, will be applied to all
+         * someother|except:index,list   // This will be only applied to posts()
+         * yet_another_one|only:index    // This will be only applied to index()
+         **/
+        return [];
+    }
+
+    /**
+     * Get the login page.
+     *
+     * @url     GET|HEAD: http://www.domain.org/login
+     * @return  blade view
+     */
+    public function index()
+    {
+        return $this->blade->render('auth/login');
+    }
+
+    /**
+     * @return redirect|response
+     */
+    public function verify()
+    {
+
+    }
+
+    /**
+     * @return redirect|response
+     */
+    public function logout()
+    {
+
+    }
+
+    /**
+     * @return redirect|response
+     */
+    public function reset()
+    {
+
+    }
+
+    /**
+     * Get the creation view for a new login.
+     *
+     * @url    GET|HEAD: http://domain.org/login/create
+     * @return blade view.
+     */
+    public function create()
+    {
+        return $this->blade->render('auth/create');
+    }
+
+    /**
+     * Register new user.
+     *
+     * @url    POST: http://www.domain.tld/login/register
+     * @return redirect|response
+     */
+    public function register()
+    {
+        $this->form_validation->set_rules('name', 'Name', 'trim|required');
+        $this->form_validation->set_rules('email', 'Email', 'trim|required');
+        $this->form_validation->set_rules('username', 'Username', 'trim|required');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required');
+
+        if ($this->form_validation->run() === false) { // Validation fails
+            $class   = 'alert alert-danger';
+            $message = lang('flash-register-error');
+        } else { // Validation passes
+            $input['name']     = $this->input->post('name');
+            $input['username'] = $this->input->post('username');
+            $input['email']    = $this->input->post('email');
+            $input['password'] = md5($this->input->post('password'));
+            $input['blocked']  = 0;
+
+            if (Login::create($input)) { // The user has been created.
+                $class   = 'alert alert-success';
+                $message = lang('flash-register-success');
+            }
+        }
+
+        // Set the flash message.
+        $this->session->set_flashdata('class', $class);
+        $this->session->set_flashdata('message', $message);
+
+        redirect($_SERVER['HTTP_REFERER']);
+    }
+
+    /**
+     * Delete a user out off the system.
+     *
+     * @url    GET|HEAD: http://www.domain.tld/login/delete/{id}
+     * @return redirect|response
+     */
+    public function delete()
+    {
+        $userId = $this->uri->segment(3);
+
+        if (Login::destroy($userId)) { // The user is deleted out off the system.
+            $this->session->set_flashdata('class', 'alert alert-success');
+            $this->session->set_flashdata('message', 'De login us verwijderd.');
+        }
+    }
+}
--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -42,6 +42,6 @@ class Welcome extends CI_Controller
      */
     public function index()
     {
-        $this->blade->render();
+        return $this->blade->render();
     }
 }
