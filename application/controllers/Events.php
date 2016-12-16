1fcc0265b42b3765c16d38086d30a51a5acb651c
Tjoosten <Tjoosten3@gmail.com>
2016-12-16 11:43:12 +0100

    Add events controller

---
+++ application/controllers/Events.php
@@ -0,0 +1,78 @@
+<?php defined('BASEPATH') or exit ('No direct script access allowed');
+
+/**
+ * Events controller.
+ *
+ * @author      Tim Joosten
+ * @license     MIT License
+ * @since       2016
+ * @package     Activisme-BE Index Site
+ */
+class Events extends MY_Controller
+{
+    /**
+     * Events Constructor
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @return blade view.
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * @return blade view.
+     */
+    public function show()
+    {
+
+    }
+
+    /**
+     * @return blade view.
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * @return blade view.
+     */
+    public function edit()
+    {
+
+    }
+
+    /**
+     * @return redirect|response
+     */
+     public function update()
+     {
+         
+     }
+
+    /**
+     * @return redirect|response
+     */
+    public function store()
+    {
+
+    }
+
+    /**
+     * @return redirect|response
+     */
+    public function destroy()
+    {
+
+    }
+}
